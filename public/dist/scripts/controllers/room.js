7c5d50fdbed38ed995ba2940272b2a225fccb381
kaflan <kaf_90@mail.ru>
2015-12-13 02:06:38 +0200

    add

--- public/dist/scripts/controllers/room.js
+++ public/dist/scripts/controllers/room.js
@@ -8,25 +8,110 @@
  * Controller of the publicApp
  */
 angular.module('publicApp')
-  .controller('RoomCtrl', function($scope, $sce) {
-    var webrtc = new SimpleWebRTC({
-      // the id/element dom element that will hold "our" video
-      localVideoEl: 'localVideo',
-      // the id/element dom element that will hold remote videos
-      remoteVideosEl: 'remoteVideos',
-      // immediately ask for camera access
-      autoRequestMedia: true
+  .controller('RoomCtrl', function($scope, $sce, send) {
+    $scope.videostreams = {};
+
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+  var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: '', //remotesVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true
+  });
+
+  // we have to wait until it's ready
+  webrtc.on('readyToCall', function () {
+    webrtc.joinRoom('codepen');
+  });
+
+
+  console.log('webrtc.connection', webrtc.connection);
+
+  webrtc.connection.on('message', function(message) {
+    console.log('connection.message', message);
+  });
+
+  webrtc.on('message', function(message) {
+    console.log('webrtc.message', message);
+  });
+
+  webrtc.on('videoAdded', function (video, peer) {
+    console.log('video added', peer, video);
+
+    $scope.videostreams[video.id] =  $sce.trustAsResourceUrl(video.getAttribute('src'));
+
+    $scope.$apply();
+  });
+
+  webrtc.on('videoRemoved', function (video, peer) {
+    console.log('video removed ', video);
+    delete $scope.videostreams[video.id];
+    $scope.$apply();
+  });
+
+
+  //
+  // Chat features
+  //
+
+  $scope.userId = "User" + Math.round(Math.random() * 1000);
+	$scope.messages = ['Welcome to ____'];
+
+  // Create a publish() function in the scope
+  $scope.publish = function() {
+    console.log('publish');
+    /*
+    webrtc.connection.emit('message', {
+      message: "[" + $scope.userId + "] " + $scope.newMessage,
+      type: 'chat'
     });
+    */
+    webrtc.sendToAll('chat', {
+      message: "[" + $scope.userId + "] " + $scope.newMessage
+    });
+
+    $scope.newMessage = '';
+  };
+
+  //webrtc.connection.on('message', function(message) {
+  webrtc.connection.on('message', function(message) {
+		if(message.type !== "chat") return;
+
+    /*
+    console.log('message.type', message.type, message);
+
+    console.log('chat', message);
+    message = message.message;
+    $scope.$apply(function() {
+      if ($scope.messages.indexOf(message) == -1) {
+        $scope.messages.unshift(message);
+      }
+    });
+    */
+    message = message.payload.message;
+    //message = JSON.stringify(message);
+    $scope.$apply(function() {
+      if ($scope.messages.indexOf(message) == -1) {
+        $scope.messages.unshift(message);
+      }
+    });
+
+  });
+
     $scope.close = function() {
 
     };
     $scope.call = function() {
       // webrtc.on('readyToCall', function() {
       // you can name it anything
+
       webrtc.joinRoom('your awesome room name');
       // });
     }
-    $scope.getLocalVideo = function() {
-      return $sce.trustAsResourceUrl();
-    };
+    $scope.getLocalVideo()
   });
--- public/dist/scripts/services/send.js
+++ public/dist/scripts/services/send.js
@@ -8,10 +8,34 @@
  * Service in the publicApp.
  */
 angular.module('publicApp')
-  .service('send', function ($http) {
-    return {
-      query: function(url, data){
-        return $http.post(url, data);
-      }
+  .service('send', function($q) {
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    var constraints = {
+      audio: false,
+      video: true
     };
+
+    var deferred = $q.defer();
+
+    var get = function() {
+      navigator.getUserMedia(
+        constraints,
+        function(stream) {
+          deferred.resolve(stream);
+        },
+        function errorCallback(error) {
+          console.log('navigator.getUserMedia error: ', error);
+          deferred.reject(error);
+        }
+      );
+
+      return deferred.promise;
+    }
+
+    return {
+      get: get
+    }
+
   });
